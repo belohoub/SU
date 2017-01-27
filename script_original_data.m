bfff73f8ef39c50069b33c05772a230704fd969d
zikaj <j.zika23@gmail.com>
2017-01-27 20:03:33 +0100

    Added libsvm with examples of training

--- script_original_data.m
+++ script_original_data.m
@@ -9,6 +9,12 @@ pkg load io
 # !!!CHECK CORECT PATH!!!
 data = csv2cell('/home/dzejkob23/Documents/Git/SU/titanic.csv');
 
+#####################################
+########## ADD SVM LIBRARY ##########
+#####################################
+
+# type "make" in ./libsvm-3.22/matlab
+# type => "addpath './libsvm-3.22/matlab'";
 
 ########################################
 ########## DATA PREPROCESSING ##########
@@ -68,7 +74,7 @@ endfor
 ########## SCALE ALL SYMPTOMS ###########
 #########################################     
     
-for i = 1:9
+for i = 2:9
   for j = 1:count
     scaled_training_set(j,i) = training_set(j,i) / max(training_set(:,i));
   endfor
@@ -79,8 +85,7 @@ endfor
 ########################################    
 
 # INITIALIZE THETA AND MATRIX OF SIMILARITY
-theta_vector = [1;1;1;1;1;1;1;1;1];
-# f = zeros (count, count, "uint8");
+f_int = zeros (count, count, "uint8");
 
 # SIGMA
 sigma = 1;
@@ -93,10 +98,51 @@ for i = 1:count
     x_2 = scaled_training_set(j,:);
     
     # similarity of sample X is in ROW
-    f(i,j) = exp(-norm(x_1 - x_2)^2 / 2 * sigma^2);
+    # rounding to DOUBLE
+    f_double(i,j) = exp(-norm(x_1 - x_2)^2 / 2 * sigma^2);
+    # rounding to INTEGER
+    f_int(i,j) = f_double(i,j);
   endfor
 endfor
 
-# CREATE LANDMARKS
-# NOMALIZE LANDAMRKS/X
-# COMPUTE KERNELS BY LIBSVM OR OTHER LIBRARY (KERNEL, EXP, SIMILARITY, ...)
+##########################################
+########## TRAINING WITH LIBSVM ##########
+##########################################
+
+# get answares from teacher
+answers_y = scaled_training_set(:,1);
+# drop answers from training set
+scaled_training_set (:,1) = [];
+
+# Usage: model = svmtrain(training_label_vector, training_instance_matrix, 'libsvm_options');
+# libsvm_options:
+# -s svm_type : set type of SVM (default 0)
+#         0 -- C-SVC              (multi-class classification)
+#         1 -- nu-SVC             (multi-class classification)
+#         2 -- one-class SVM
+#         3 -- epsilon-SVR        (regression)
+#         4 -- nu-SVR             (regression)
+# -t kernel_type : set type of kernel function (default 2)
+#         0 -- linear: u'*v
+#         1 -- polynomial: (gamma*u'*v + coef0)^degree
+#         2 -- radial basis function: exp(-gamma*|u-v|^2)
+#         3 -- sigmoid: tanh(gamma*u'*v + coef0)
+#         4 -- precomputed kernel (kernel values in training_instance_matrix)
+# -d degree : set degree in kernel function (default 3)
+# -g gamma : set gamma in kernel function (default 1/num_features)
+# -r coef0 : set coef0 in kernel function (default 0)
+# -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)
+# -n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5)
+# -p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1)
+# -m cachesize : set cache memory size in MB (default 100)
+# -e epsilon : set tolerance of termination criterion (default 0.001)
+# -h shrinking : whether to use the shrinking heuristics, 0 or 1 (default 1)
+# -b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)
+# -wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)
+# -v n : n-fold cross validation mode
+# -q : quiet mode (no outputs)
+
+# EXAMPLES:
+# SVMStruct = svmtrain(answers_y,scaled_training_set);
+# SVMStruct = svmtrain(answers_y,scaled_training_set,'['-s 3 -t 2 -c 1 -p 0.001 -g 1 -v 5']');
+
